2627f4d3a0cfc214b4dc7ce4d72894f4c98ee9fd
frontrider <kisandrasgabor@gmail.com>
2019-01-12 11:19:29 +0100

    updated the buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,25 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.3.11"
-    id("com.github.johnrengelman.shadow") version "4.0.3"
+    application
+    id("com.jfrog.artifactory") version "4.9.0"
+    `maven-publish`
     id("org.jetbrains.dokka") version "0.9.17"
 }
 
 group = "io.github.cottonmc"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     jcenter()
 }
 
+application {
+    mainClassName = "io.github.cottonmc.jsonfactory.Mainkt"
+}
+
 dependencies {
+    compile("com.beust:klaxon:5.0.1")
     api(kotlin("stdlib-jdk8"))
     api("com.google.code.gson:gson:2.8.5")
     implementation("com.miglayout:miglayout-swing:5.2")
@@ -38,5 +45,27 @@ tasks.withType<DokkaTask> {
 tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "io.github.cottonmc.jsonfactory.MainKt"
+    }
+}
+
+
+tasks.create<Zip>("sourcesJar") {
+    extension = "jar"
+    classifier = "sources"
+    from("src/main/kotlin")
+    from("src/main/resources")
+
+}
+//the artifactory block is written in the groovy dsl
+apply(from="artifactory.gradle")
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifact(tasks["distZip"])
+            artifact(tasks["sourcesJar"])
+        }
     }
 }
