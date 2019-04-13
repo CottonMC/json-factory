9b65ce334364f92baf8abafde0666020344fbe5c
Juuxel <kasperi.kauppi@gmail.com>
2019-04-13 21:20:14 +0300

    Switch to sets in BlockStateProperty

--- src/main/kotlin/io/github/cottonmc/jsonfactory/data/BlockStateProperty.kt
+++ src/main/kotlin/io/github/cottonmc/jsonfactory/data/BlockStateProperty.kt
@@ -4,9 +4,9 @@ package io.github.cottonmc.jsonfactory.data
  * A block state property.
  *
  * @property name the name
- * @property values a list of all values
+ * @property values a set of all values
  */
-data class BlockStateProperty(val name: String, val values: List<String>) {
+data class BlockStateProperty(val name: String, val values: Set<String>) {
     override fun equals(other: Any?) =
         other is BlockStateProperty && other.name == name
 
@@ -27,21 +27,21 @@ data class BlockStateProperty(val name: String, val values: List<String>) {
     }
 
     companion object {
-        val axis = BlockStateProperty("axis", listOf("x", "y", "z"))
-        val horizontalFacing = BlockStateProperty("facing", listOf("north", "east", "south", "west"))
-        val halfTB = BlockStateProperty("half", listOf("bottom", "top"))
-        val halfUL = BlockStateProperty("half", listOf("upper", "lower"))
-        val slabType = BlockStateProperty("type", listOf("bottom", "top", "double"))
-        val stairShape = BlockStateProperty("shape", listOf("straight", "outer_right", "outer_left", "inner_right", "inner_left"))
-        val buttonFace = BlockStateProperty("face", listOf("floor", "wall", "ceiling"))
+        val axis = BlockStateProperty("axis", setOf("x", "y", "z"))
+        val horizontalFacing = BlockStateProperty("facing", setOf("north", "east", "south", "west"))
+        val halfTB = BlockStateProperty("half", setOf("bottom", "top"))
+        val halfUL = BlockStateProperty("half", setOf("upper", "lower"))
+        val slabType = BlockStateProperty("type", setOf("bottom", "top", "double"))
+        val stairShape = BlockStateProperty("shape", setOf("straight", "outer_right", "outer_left", "inner_right", "inner_left"))
+        val buttonFace = BlockStateProperty("face", setOf("floor", "wall", "ceiling"))
         val powered = boolean("powered")
         val gateInWall = boolean("in_wall")
         val open = boolean("open")
-        val doorHinge = BlockStateProperty("hinge", listOf("left", "right"))
+        val doorHinge = BlockStateProperty("hinge", setOf("left", "right"))
 
         /**
          * Creates a [BlockStateProperty] with the [name] and the two boolean values: `true` and `false`.
          */
-        fun boolean(name: String) = BlockStateProperty(name, listOf("true", "false"))
+        fun boolean(name: String) = BlockStateProperty(name, setOf("true", "false"))
     }
 }
