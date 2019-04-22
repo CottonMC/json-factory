7e7f870acaad3f91f9caa1e2ac68949dbd811e07
Juuxel <kasperi.kauppi@gmail.com>
2019-04-22 10:48:42 +0300

    Add classpath plugin loader for GUI

--- gui/src/main/kotlin/io/github/cottonmc/jsonfactory/gui/Main.kt
+++ gui/src/main/kotlin/io/github/cottonmc/jsonfactory/gui/Main.kt
@@ -2,22 +2,39 @@ package io.github.cottonmc.jsonfactory.gui
 
 import io.github.cottonmc.jsonfactory.gens.ContentGenerator
 import io.github.cottonmc.jsonfactory.gens.Gens
-import io.github.cottonmc.jsonfactory.plugin.JarPluginLoader
-import io.github.cottonmc.jsonfactory.plugin.PluginLoadingContext
-import io.github.cottonmc.jsonfactory.plugin.PluginManager
+import io.github.cottonmc.jsonfactory.plugin.*
 import java.io.File
+import kotlin.reflect.KClass
 
-fun main() {
+fun main(args: Array<String>) {
     Settings.init()
     // TODO: Show a splash screen or something during plugin loading
-    Gui.createAndShow(loadGens())
+    // TODO: Document CLI args
+    Gui.createAndShow(loadGens(args))
 }
 
-private fun loadGens(): List<ContentGenerator> = sequence {
+private fun loadGens(classes: Array<String>): List<ContentGenerator> = sequence {
     yieldAll(Gens.allGens)
-    yieldAll(PluginManager.loadPlugins(
-        JarPluginLoader(), PluginLoadingContext(File("."))
-    ).flatMap {
-        it.plugin.generators
-    })
+
+    val plugins = sequence {
+        yieldAll(
+            PluginManager.loadPlugins(
+                JarPluginLoader(), PluginLoadingContext(File("."))
+            )
+        )
+
+        @Suppress("UNCHECKED_CAST")
+        yieldAll(
+            PluginManager.loadPlugins(
+                ClasspathPluginLoader(
+                    classes.map {
+                        Class.forName(it).kotlin as KClass<out Plugin>
+                    }
+                ),
+                PluginLoadingContext(File("."))
+            )
+        )
+    }
+
+    yieldAll(plugins.flatMap { it.plugin.generators.asSequence() })
 }.toList()
---
+++ src/main/kotlin/io/github/cottonmc/jsonfactory/plugin/ClasspathPluginLoader.kt
@@ -0,0 +1,11 @@
+package io.github.cottonmc.jsonfactory.plugin
+
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+
+class ClasspathPluginLoader(private val classes: List<KClass<out Plugin>>) : PluginLoader {
+    override fun loadPlugins(context: PluginLoadingContext) =
+        classes.mapIndexed { index, clazz ->
+            PluginContainer(clazz.objectInstance ?: clazz.createInstance(), "plugin_${hashCode()}_$index", "1")
+        }
+}
