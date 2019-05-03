40f15b035d41fd9eb10143f627d994062c970083
Juuxel <kasperi.kauppi@gmail.com>
2019-05-03 18:42:51 +0300

    Fix indentation in fake AWTUtilities
    This is what contributing to Fabric does to your IDE.

--- gui/src/main/java/com/sun/awt/AWTUtilities.java
+++ gui/src/main/java/com/sun/awt/AWTUtilities.java
@@ -6,12 +6,12 @@ import java.awt.*;
  * Workaround for CottonMC/json-factory#7, part 2
  */
 public class AWTUtilities {
-	public static boolean isTranslucencySupported(Translucency translucency) {
-		return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
-				.isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.PERPIXEL_TRANSLUCENT);
-	}
+    public static boolean isTranslucencySupported(Translucency translucency) {
+        return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+                .isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.PERPIXEL_TRANSLUCENT);
+    }
 
-	public enum Translucency {
-		PERPIXEL_TRANSPARENT
-	}
+    public enum Translucency {
+        PERPIXEL_TRANSPARENT
+    }
 }
