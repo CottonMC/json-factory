656d9ee55f2b0c171efd5bbea582aff24be3f859
Juuxel <kasperi.kauppi@gmail.com>
2019-05-29 17:49:48 +0300

    Switch to throws() in AssertionsKt.isFailure()

--- src/test/kotlin/io/github/cottonmc/jsonfactory/tests/util/Assertions.kt
+++ src/test/kotlin/io/github/cottonmc/jsonfactory/tests/util/Assertions.kt
@@ -2,10 +2,12 @@ package io.github.cottonmc.jsonfactory.tests.util
 
 import arrow.core.Option
 import arrow.core.Try
+import arrow.core.orNull
 import strikt.api.Assertion
+import strikt.assertions.throws
 
 fun <A> Assertion.Builder<Try<A>>.isSuccess() = assertThat("has successfully returned") { it.isSuccess() }
-fun <A> Assertion.Builder<Try<A>>.isFailure() = assertThat("has thrown an exception") { it.isFailure() }
+fun <A> Assertion.Builder<Try<A>>.isFailure() = get { failed().orNull() }.throws<Throwable>()
 
 fun <A> Assertion.Builder<Option<A>>.isSome() = assertThat("is present") { it.isDefined() }
 fun <A> Assertion.Builder<Option<A>>.isNone() = assertThat("is not present") { it.isEmpty() }
