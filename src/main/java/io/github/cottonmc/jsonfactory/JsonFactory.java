221d8f6f1ab696c9bb89ab4c5ca6dddbd4dcc8b2
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-01-09 19:18:58 +0200

    Add "main class" for generating data into a directory

---
+++ src/main/java/io/github/cottonmc/jsonfactory/JsonFactory.java
@@ -0,0 +1,46 @@
+package io.github.cottonmc.jsonfactory;
+
+import io.github.cottonmc.jsonfactory.generator.ContentGenerator;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Set;
+
+public final class JsonFactory {
+    private final ContentGenerator generator;
+
+    public JsonFactory(Iterable<? extends ContentGenerator> generators) {
+        Objects.requireNonNull(generators, "generators");
+        this.generator = new MultiContentGenerator(generators);
+    }
+
+    public JsonFactory(ContentGenerator generator) {
+        Objects.requireNonNull(generator, "generator");
+        this.generator = generator;
+    }
+
+    public Set<GenerationResult> generate(GenerationContext context) {
+        Objects.requireNonNull(context, "context");
+        return generator.generate(context);
+    }
+
+    public void generateTo(Path directory, GenerationContext context) throws IOException {
+        Objects.requireNonNull(directory, "directory");
+        Objects.requireNonNull(context, "context");
+
+        if (Files.exists(directory) && !Files.isDirectory(directory)) {
+            throw new IOException("Output path '" + directory + "' is not a directory");
+        }
+
+        for (GenerationResult result : generate(context)) {
+            Path target = directory.resolve(result.getPath().replace("/", directory.getFileSystem().getSeparator()));
+
+            // Create missing containing directories
+            Files.createDirectories(target.getParent());
+            Files.write(target, result.getData().getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
