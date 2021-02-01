be15e93bdb3092540189258e8ae4358630c9623b
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-02-01 23:14:09 +0200

    Document ContentGenerator subclasses

--- src/main/java/io/github/cottonmc/jsonfactory/generator/MultiContentGenerator.java
+++ src/main/java/io/github/cottonmc/jsonfactory/generator/MultiContentGenerator.java
@@ -3,20 +3,34 @@ package io.github.cottonmc.jsonfactory.generator;
 import io.github.cottonmc.jsonfactory.GenerationResult;
 import io.github.cottonmc.jsonfactory.context.GenerationContext;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import static org.organicdesign.fp.StaticImports.set;
+
+/**
+ * A content generator that groups multiple generators into one.
+ */
 public final class MultiContentGenerator implements ContentGenerator {
     private final Iterable<? extends ContentGenerator> generators;
 
+    /**
+     * Constructs a new grouping content generator from an iterable of content generators.
+     *
+     * @param generators the generators
+     */
     public MultiContentGenerator(Iterable<? extends ContentGenerator> generators) {
         this.generators = Objects.requireNonNull(generators, "generators");
     }
 
+    /**
+     * Constructs a new grouping content generator from an array of content generators.
+     *
+     * @param generators the generators
+     */
     public MultiContentGenerator(ContentGenerator... generators) {
-        this(new HashSet<>(Arrays.asList(generators)));
+        this(set(Objects.requireNonNull(generators, "generators")));
     }
 
     @Override
--- src/main/java/io/github/cottonmc/jsonfactory/generator/MustacheContentGenerator.java
+++ src/main/java/io/github/cottonmc/jsonfactory/generator/MustacheContentGenerator.java
@@ -11,10 +11,25 @@ import java.util.Set;
 
 import static org.organicdesign.fp.StaticImports.vec;
 
+/**
+ * A content generator that uses {@linkplain Template Mustache templates} for generation.
+ *
+ * <p>Mustache generators use two separate {@link Template} objects: one of the content and one for the content's path.
+ * Both of the templates are evaluated using a {@link GenerationContext} to create the data.
+ *
+ * <p>The path template should use {@code /} at its path component separator (as in {@code file/in/directory}.
+ * The separators will be converted to correct platform-specific ones with the path methods in {@link GenerationPath}.
+ */
 public final class MustacheContentGenerator implements ContentGenerator {
     private final Template pathTemplate;
     private final Template contentTemplate;
 
+    /**
+     * Constructs a Mustache-based content generator.
+     *
+     * @param pathTemplate    the template used for content paths
+     * @param contentTemplate the template used for content
+     */
     public MustacheContentGenerator(Template pathTemplate, Template contentTemplate) {
         this.pathTemplate = Objects.requireNonNull(pathTemplate, "pathTemplate");
         this.contentTemplate = Objects.requireNonNull(contentTemplate, "contentTemplate");
